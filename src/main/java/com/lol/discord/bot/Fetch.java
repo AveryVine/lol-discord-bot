53772f3ecbd65feac8a173cfa6e2fea7f27917ac
Avery Vine <averyvine@icloud.com>
2017-12-02 15:23:34 -0500

    Added first network call plus error handling

--- src/main/java/com/lol/discord/bot/Fetch.java
+++ src/main/java/com/lol/discord/bot/Fetch.java
@@ -1,13 +1,14 @@
 package com.lol.discord.bot;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.charset.Charset;
 
 import org.json.*;
@@ -18,33 +19,54 @@ public class Fetch {
 	private final Parse parse = new Parse();
 
 	public String retrieveData(String keyword, String body) {
-		String message = "Something went wrong!";
+		String message = "";
 		if ("lookup".equalsIgnoreCase(keyword)) {
 			message = "";
 			try {
-				JSONObject json = readJsonFromUrl(
-						"https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + body + "?api_key=" + apiKey);
-				message = json.toString();
+				if (body == null || "".equals(body)) {
+					message = "Not a valid summoner name!";
+				}
+				URI uri = new URI("https", "na1.api.riotgames.com", "/lol/summoner/v3/summoners/by-name/" + body,
+						"api_key=" + apiKey, null);
+				URL url = uri.toURL();
+				String jsonText = readJsonFromUrl(url);
+				if ("Error".equals(jsonText)) {
+					message = "Invalid summoner name!";
+				}
+				else {
+					message = jsonText;
+					// JSONObject json = new JSONObject(jsonText);
+					// message = json.toString();
+				}
 			} catch (Exception e) {
-				message = "Something went wrong!\n" + e.getStackTrace();
+				message = "Something went wrong!\n" + e.getStackTrace().toString();
 			}
 		}
+		System.out.println("Returning message");
 		return message;
 	}
 
-	public JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-		System.out.println("Reaching out to url: " + url);
-		InputStream is = new URL(url).openStream();
-		try {
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-			System.out.println("Reading in...");
-			String jsonText = readAll(rd);
-			System.out.println("Received: " + jsonText);
-			JSONObject json = new JSONObject(jsonText);
-			return json;
-		} finally {
-			is.close();
+	public String readJsonFromUrl(URL url) throws IOException, JSONException {
+		System.out.println("Attempting to reach url: " + url);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		final int responseCode = connection.getResponseCode();
+		switch (responseCode) {
+		case 404:
+			return "Error";
+		case 200:
+			InputStream is = url.openStream();
+			try {
+				BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+				System.out.println("Reading in...");
+				String jsonText = readAll(rd);
+				System.out.println("Received: " + jsonText);
+				return jsonText;
+			} finally {
+				is.close();
+			}
 		}
+		return "Something went wrong!";
+		
 	}
 
 	private static String readAll(Reader rd) throws IOException {
--- src/main/java/com/lol/discord/bot/LolDiscordBot.java
+++ src/main/java/com/lol/discord/bot/LolDiscordBot.java
@@ -7,36 +7,36 @@ import de.btobastian.javacord.Javacord;
 import de.btobastian.javacord.entities.message.Message;
 import de.btobastian.javacord.listener.message.MessageCreateListener;
 
-/**
- * A simple ping-pong bot.
- */
 public class LolDiscordBot {
 
-    public static void main(String args[]) {
-    		final Fetch fetch = new Fetch();
-    		String token = "Mzg2NTc2MzM4Mjg4NTA4OTI4.DQR6zA.N74UuXBlgjWApmBFnMRiHITTcnw";
-        // See "How to get the token" below
-        DiscordAPI api = Javacord.getApi(token, true);
-        // connect
-        api.connect(new FutureCallback<DiscordAPI>() {
-            public void onSuccess(DiscordAPI api) {
-                // register listener
-                api.registerListener(new MessageCreateListener() {
-                    public void onMessageCreate(DiscordAPI api, Message message) {
-                        // check the content of the message
-                    		String msg = message.getContent();
-                    		String keyword = msg.substring(0, msg.indexOf(" ")).trim();
-                    		String body = msg.substring(msg.indexOf(" ") + 1, msg.length()).trim();
-                    		String output = fetch.retrieveData(keyword, body);
-                        message.reply(output);
-                    }
-                });
-            }
+	public static void main(String args[]) {
+		final Fetch fetch = new Fetch();
+		String token = "Mzg2NTc2MzM4Mjg4NTA4OTI4.DQR6zA.N74UuXBlgjWApmBFnMRiHITTcnw";
+		// See "How to get the token" below
+		DiscordAPI api = Javacord.getApi(token, true);
+		// connect
+		api.connect(new FutureCallback<DiscordAPI>() {
+			public void onSuccess(DiscordAPI api) {
+				// register listener
+				api.registerListener(new MessageCreateListener() {
+					public void onMessageCreate(DiscordAPI api, Message message) {
+						// check the content of the message
+						System.out.println("Incoming message: " + message.getContent());
+						String msg = message.getContent().trim();
+						if (msg.indexOf(" ") != -1) {
+							String keyword = msg.substring(0, msg.indexOf(" ")).trim();
+							String body = msg.substring(msg.indexOf(" ") + 1, msg.length()).trim();
+							String output = fetch.retrieveData(keyword, body);
+							message.reply(output);
+						}
+					}
+				});
+			}
 
-            public void onFailure(Throwable t) {
-                t.printStackTrace();
-            }
-        });
-    }
+			public void onFailure(Throwable t) {
+				t.printStackTrace();
+			}
+		});
+	}
 
 }
