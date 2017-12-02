72e1312e7f0dea78ee4c92d949dcbe37dda08015
Avery Vine <averyvine@icloud.com>
2017-12-02 12:19:10 -0500

    working build configuration

---
+++ src/main/java/com/lol/discord/bot/LolDiscordBot.java
@@ -0,0 +1,40 @@
+package com.lol.discord.bot;
+
+import com.google.common.util.concurrent.FutureCallback;
+
+import de.btobastian.javacord.DiscordAPI;
+import de.btobastian.javacord.Javacord;
+import de.btobastian.javacord.entities.message.Message;
+import de.btobastian.javacord.listener.message.MessageCreateListener;
+
+/**
+ * A simple ping-pong bot.
+ */
+public class LolDiscordBot {
+
+    public static void main(String args[]) {
+    		String token = "";
+        // See "How to get the token" below
+        DiscordAPI api = Javacord.getApi(token, true);
+        // connect
+        api.connect(new FutureCallback<DiscordAPI>() {
+            public void onSuccess(DiscordAPI api) {
+                // register listener
+                api.registerListener(new MessageCreateListener() {
+                    public void onMessageCreate(DiscordAPI api, Message message) {
+                        // check the content of the message
+                        if (message.getContent().equalsIgnoreCase("ping")) {
+                            // reply to the message
+                            message.reply("pong");
+                        }
+                    }
+                });
+            }
+
+            public void onFailure(Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+
+}
